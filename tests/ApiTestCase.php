960429d714c4f707c79c38882d51a2dd2487b5f8
Nicolas_R <nicolas.rapisarda@meetpro.fr>
2020-06-22 20:04:51 +0200

    login method test

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,6 +2,24 @@
 
 namespace App\Tests;
 
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
+
 class ApiTestCase extends \ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase
 {
+    protected static ?string $token = null;
+
+    protected static function createClient(array $kernelOptions = [], array $defaultOptions = []): Client
+    {
+        if (null !== static::$token) {
+            $defaultOptions = array_merge($defaultOptions, ['Authorization' => ['Bearer '.static::$token]]);
+        }
+
+        return parent::createClient($kernelOptions, $defaultOptions);
+    }
+
+    protected static function login(array $parameters): void
+    {
+        $body = static::createClient()->request('POST', '/authentication_token', ['json' => $parameters])->toArray();
+        static::$token = $body['token'];
+    }
 }
