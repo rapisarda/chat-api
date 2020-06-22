47eecf718f4849a4e068e7edfe31d1c5542912d7
Nicolas_R <nicolas.rapisarda@meetpro.fr>
2020-06-22 19:35:38 +0200

    add dama/doctrine-test-bundle

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,8 +2,6 @@
 
 namespace App\Tests;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestAssertionsTrait;
-
 class ApiTestCase extends \ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase
 {
 }
